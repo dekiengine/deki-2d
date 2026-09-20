dd806e4a7d1ad91f276275721e666d1b0db95c29
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-20 21:03:45 +0200

    Docs for deki-2d from deki-editor d152da0

--- navtreedata.js
+++ navtreedata.js
@@ -41,9 +41,11 @@ var NAVTREE =
       [ "TextComponent", "index.html#c-textcomponent", null ]
     ] ],
     [ "Changelog", "index.html#autotoc_md3", [
-      [ "0.15.0", "index.html#autotoc_md4", null ],
+      [ "0.16.0", "index.html#autotoc_md4", null ],
       [ "Changed", "index.html#autotoc_md5", null ],
-      [ "Fixed", "index.html#autotoc_md6", null ]
+      [ "0.15.0", "index.html#autotoc_md6", null ],
+      [ "Changed", "index.html#autotoc_md7", null ],
+      [ "Fixed", "index.html#autotoc_md8", null ]
     ] ]
   ] ]
 ];
--- navtreeindex0.js
+++ navtreeindex0.js
@@ -8,6 +8,8 @@ var NAVTREEINDEX0 =
 "index.html#autotoc_md4":[3,0],
 "index.html#autotoc_md5":[3,1],
 "index.html#autotoc_md6":[3,2],
+"index.html#autotoc_md7":[3,3],
+"index.html#autotoc_md8":[3,4],
 "index.html#c-animationcomponent":[2,0],
 "index.html#c-buttoncomponent":[2,1],
 "index.html#c-buttonstylecomponent":[2,2],
